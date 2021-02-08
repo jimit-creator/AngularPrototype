771ebcaf31e9ed58ed10a4d7037a97428fdbff3b
jimit-creator <45489578+jimit-creator@users.noreply.github.com>
2021-02-08 18:45:25 +0530

    Project convert SCSS base.

---
+++ replace.build.js
@@ -0,0 +1,29 @@
+/*var replace = require("replace-in-file");
+var package = require("package.json");
+var buildVersion = package.version;
+const options = {
+  files: "src/environments/environment.prod.ts",
+  from: /version: '(.*)'/g,
+  to: "version: '" + buildVersion + "'",
+  allowEmptyPaths: false
+};
+
+try {
+  let changedFiles = replace.sync(options);
+  if (changedFiles == 0) {
+    throw (
+      "Please make sure that file '" + options.files + "' has \"version: ''\""
+    );
+  }
+  let currentDate = new Date();
+  console.log(
+    "Build Time:::" +
+      currentDate.toLocaleDateString() +
+      " " +
+      currentDate.toLocaleTimeString()
+  );
+  console.log("Build version set: " + buildVersion);
+} catch (error) {
+  console.error("Error occurred:", error);
+  throw error;
+}*/
